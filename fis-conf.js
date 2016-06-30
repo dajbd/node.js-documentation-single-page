bcec2e969186d9bc927207db674ac5458b3abee3
Engin <denganjun1234@qq.com>
2016-06-30 17:48:39 +0800

    fix fis3 config; inline; dist;

--- fis-conf.js
+++ fis-conf.js
@@ -1,7 +1,16 @@
-// fis.match('*.html', {
-//     release: true
-// })
+fis.match('*', {
+    release: false
+})
 
-// fis.match('*', {
-//     release: false
-// })
+fis.match('*.htm', {
+    release: function() {
+    	// console.log('\n',JSON.stringify(arguments))
+    	/*
+    	{
+    	 	"0": ["/6.2.2/About this Documentation Node.js v6.2.2 Manual & Documentation.htm"],
+    	 	"1": "/6.2.2/About this Documentation Node.js v6.2.2 Manual & Documentation.htm"
+    	}
+    	*/
+        return arguments[1].replace('About this Documentation', '').trim()
+    }
+})
